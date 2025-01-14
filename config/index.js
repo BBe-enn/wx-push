45fcb96415579242d60dcc6f77c107ec45863a56
BBe-enn <fengbinghe2002@gmail.com>
2025-01-14 14:58:23 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -2,9 +2,9 @@ module.exports = {
   appid: "wx2ae0ff16c56123d0",
   appsecret: "ca035ffac56d562d8bae565215fce646",
   template_id: "xli79SUYxSGl1Er7VwKNgzD2aUD2oEaHziPp--Y8hNA",
-  touser: "ae58909f59f9f7813d3dd684404bc8e4",
+  touser: "oUtNJ7PRpZqz23IAqHSm44xqGBwk",
 
-  key: "201c93c2e2b34bdeecdbf9f5265eb84e",
+  key: "ae58909f59f9f7813d3dd684404bc8e4",
   city: "郑州",
   first_date: "2024-12-31",
 };
